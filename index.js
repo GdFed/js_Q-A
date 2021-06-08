48544e016fb8190e8fcc8a737fa10ebf7e238e41
xiaozhilong <xiaozhilong@huishoubao.com>
2021-06-08 17:10:57 +0800

    init

---
+++ index.js
@@ -0,0 +1,198 @@
+// dom/bom
+/*
+事件流（捕获/冒泡/事件代理/事件委托）
+html5新标签及新特性
+兼容性
+ajax
+跨域（jsonp，postmessage，ajax）
+w3c新api
+svg/canvas/webGL
+*/
+
+// css
+/*
+visibility:hidden/display:none/opacity:0
+盒子模型
+margin塌陷
+BFC（IFC）
+清除浮动
+flex
+css hack
+less/sass
+居中布局
+响应式布局
+重绘/回流
+*/
+
+// js&高级
+/*
+面向对象
+原型/原型链
+new
+构造函数（persom.__proto__===Person.prototype(__proto__不是w2c标准，但是浏览器基本上都提供)）
+继承（原型继承，构造函数继承，组合继承）
+this
+call，apply，bind
+作用域
+高阶函数（柯里化）
+闭包
+数据类型（基础/引用，==，判断类型（全部是基本类型（同类型直接比较，不同类型转为number）；含有基本类型（引用类型.valueOf()?.toString()）；不含基本类型都不==（引用指向不同的地址）））
+*/
+
+// es6+
+/*
+箭头函数
+let,const,var(变量提升)
+迭代器、生成器
+promise，async/await（异步：回调，事件监听（发布订阅））
+Proxy/Reflect
+Set/Map
+.../解构/``
+Class
+Array/Object新增方法
+*/
+
+// node
+/*
+event loop
+模块
+egg（koa，express）
+持久化（redis）
+puppeteer
+*/
+
+// 浏览器
+/*
+event loop
+url输入回车
+渲染机制
+v8
+垃圾回收机制（引用计数，标记清除，标记整理，分代回收）
+*/
+
+// vue
+/*
+vue2.0/vue3.0
+生命周期
+diff
+虚拟dom（snabbdom）
+render函数
+数据双向绑定
+v-modal
+v-if/v-for
+slot
+组件通信
+nuxt
+*/
+
+// vue-router
+/*
+vue-router源码分析
+原理（hashchange、pushstate/replacestate/popstate）
+路由守卫
+$router/$route
+按需加载路由
+*/
+
+// vuex/mobx/flux/redux
+/*
+vuex源码分析
+状态管理机制（mobx，flux，redux）
+*/
+
+// react
+/*
+hooks
+redux
+diff
+jsx
+fiber
+*/
+
+// typescript
+/*
+类型检查
+tsc/ts-node
+泛型
+*/
+
+// webpack
+/*
+编译过程
+loader
+plugin
+ast
+常见loader
+常见plugin
+vue-cli
+webpack编译优化
+*/
+
+// 工程化
+/*
+ci
+自动化测试
+git（hooks）
+部署（docker）
+代码检查（prettier，eslint，postcss，posthtml）
+babel
+shell
+*/
+
+// http
+/*
+nginx（负载均衡（ip_hash，lest_conn,backup,down,一致性hash），反向代理，动静分离）
+状态码（200,204,301,302，304,401,403,404,500）
+1.0/1.1/2.0
+http/https
+tcp/udp
+http3.0(uqic)
+DNS
+CDN
+缓存
+CORS
+安全
+5层网络结构（应用层（应用层表示层会话层）（http），传输层（tcp，udp），网络层（ip），数据链路层，物理层）
+*/
+
+// 安全
+/*
+csrf
+xss
+token/jwt/auth
+sql注入
+*/
+
+// 跨端
+/*
+rn/weex
+webview
+flutter
+jsBridge
+通信
+编译
+渲染
+*/
+
+// 解决方案
+/*
+国际化
+大文件(断点)上传
+element二次封装
+长列表
+懒加载
+首页性能优化
+devtools
+高并发集群（分库分表）
+小程序组件开发
+低代码平台开发
+性能监控平台开发
+*/
+
+// 数据结构和算法
+/*
+排序
+堆栈
+队列
+链表
+*/
