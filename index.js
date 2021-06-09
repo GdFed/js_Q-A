c45bcf48967598351cf9fc6577842c7f74ba6a92
xzler <gdfed@foxmail.com>
2021-06-10 00:09:29 +0800

    06100009

--- index.js
+++ index.js
@@ -145,24 +145,25 @@ shell
 nginx（负载均衡（ip_hash，lest_conn,backup,down,一致性hash），反向代理，动静分离）
 状态码（200,204,301,302，304,401,403,404,500）
 1.0/1.1/2.0
-http/https
+http/https（http+ssl，对称加密，非对称加密，数字证书）
 tcp/udp
 http3.0(uqic)
-DNS
-CDN
-缓存
+DNS（域名解析系统）
+CDN（内容分发网络）
+缓存（强缓存：expries，cache-control；协商缓存：last-modified，etag）
 CORS
-安全
+安全（中间人攻击：伪造证书）
 5层网络结构（应用层（应用层表示层会话层）（http），传输层（tcp，udp），网络层（ip），数据链路层，物理层）
 websocket
 */
 
 // 安全
 /*
-csrf
-xss
+csrf（token）
+xss（csp内容安全协议 / cookie httpOnly）
 token/jwt/auth
 sql注入
+
 */
 
 // 跨端
@@ -183,6 +184,7 @@ jsBridge
 element二次封装
 长列表
 懒加载
+单点登陆
 首页性能优化
 devtools
 高并发集群（分库分表）
