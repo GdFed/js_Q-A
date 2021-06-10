e09f227b44f9e96d5c4433b1f0e0dfe3bafbd824
xiaozhilong <xiaozhilong@huishoubao.com>
2021-06-10 17:43:01 +0800

    2021-06-10 17:43:01

--- git.sh
+++ git.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
-if [ $1 = "1" ]; then
+if [ $1 = "1" ]
+then
 echo "edit readme: "$1 
 node ./index.js
 fi
 
 git add .
-if [ -n $2 ]; then
+
+if [ -n "$2" ]
+then
 echo "custom commit: "$2
 git commit -m "$2"
 else
 dt=$(date +"%Y-%m-%d %H:%M:%S")
 echo "date commit: "$dt
 fi
+
 git commit -m "$dt"
 git push
 # ./git.sh
