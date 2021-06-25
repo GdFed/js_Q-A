cd12cf99ca7af0094c85e6cce708b88667651ba4
xiaozhilong <xiaozhilong@huishoubao.com>
2021-06-25 16:24:38 +0800

    both

--- git.sh
+++ git.sh
@@ -16,10 +16,9 @@ echo "date commit: "$dt
 git commit -m "$dt"
 fi
 
-echo "$?" != "0"
-# if [[ "$?" != "0" ]]; then
-#   echo "No update !"
-# fi
+if [[ "$?" != "0" ]]; then
+  echo "No update !"
+fi
 
 # if [[ $3 = "gitee" ]]
 # then
