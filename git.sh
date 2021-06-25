34dcab27047b34eee9572c836ce34b53c2456b5e
xiaozhilong <xiaozhilong@huishoubao.com>
2021-06-25 16:32:32 +0800

    2021-06-25 16:32:32

--- git.sh
+++ git.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 if [[ $1 = "1" ]]
 then
-echo "edit readme: "$1 
+echo "-------- edit readme: "$1 
 node ./index.js
 fi
 
 git add .
 if [ -n "$2" ]
 then
-echo "custom commit: "$2
+echo "-------- custom commit: "$2
 git commit -m "$2"
 else
 dt=$(date +"%Y-%m-%d %H:%M:%S")
-echo "date commit: "$dt
+echo "-------- date commit: "$dt
 git commit -m "$dt"
 fi
 
 if [[ "$?" != "0" ]]; then
-  echo "No update !"
+  echo "-------- No update ! --------"
   exit
 fi
 
 if [[ $3 = "gitee" ]]
 then
-echo "push remote gitee !"
+echo "-------- push remote gitee ! --------"
 git push gitee master
 elif [[ $3 = "origin" ]]
 then
-echo "push remote github !"
+echo "-------- push remote github ! --------"
 git push origin master
 else
-echo "push remote both !"
+echo "-------- push remote both ! --------"
 git push gitee master
 git push origin master
 fi
